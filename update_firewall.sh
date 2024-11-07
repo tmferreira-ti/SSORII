d5f587e4dffea698e719685fd1708ad6a760dbfc
Tiago Martins Ferreira <163651394+tmferreira-ti@users.noreply.github.com>
2024-11-06 21:48:47 -0300

    Update update_firewall.sh

--- update_firewall.sh
+++ update_firewall.sh
@@ -3,11 +3,15 @@ wget https://raw.githubusercontent.com/tmferreira-ti/ASOR/main/Confs/srv1/sysctl
 
 wget https://raw.githubusercontent.com/tmferreira-ti/SSORII/refs/heads/main/Confs/firewall -O /etc/init.d/firewall --no-check-certificate --no-cache
 
-wget https://raw.githubusercontent.com/tmferreira-ti/SSORII/refs/heads/main/Confs/firewall.service -O /etc/systemd/system/firewall.service
+wget https://raw.githubusercontent.com/tmferreira-ti/SSORII/refs/heads/main/Confs/firewall.service -O /etc/systemd/system/firewall.service --no-check-certificate --no-cache
 
 chmod +x /etc/init.d/firewall
 
 systemctl enable firewall.service
+
+apt install squid
+
+wget https://raw.githubusercontent.com/tmferreira-ti/SSORII/refs/heads/main/Confs/squid.conf -O /etc/squid/squid.conf --no-check-certificate --no-cache
 
 echo "Finalizado..."
 sleep 1
